17b59e2c83e1f2e0187cd14a72895af40ea9f68d
Reece Tunesi <rtunesi2000@gmail.com>
2025-04-24 15:19:10 +0100

    Added comments to capital-letter.js

--- JavaScript Learning/capital-letter.js
+++ JavaScript Learning/capital-letter.js
@@ -1,6 +1,17 @@
+// 1. Get users name.
 var nameInput = prompt("What is your name?");
+
+// 2. Get the beginning letter of the name.
 var beginningLetter = nameInput.slice(0, 1);
+
+// 3. Capitalise the first letter of the name.
 var capitalLetter = beginningLetter.toUpperCase();
+
+// 4. Get the rest of the inputted name.
 var restOfName = nameInput.slice(1, 99);
 
+// 5. Convert the rest of the name to lower case.
+var lowerName = restOfName.toLowerCase();
+
+// 6. Display the converted name.
 alert("Hello, " + capitalLetter + restOfName)
