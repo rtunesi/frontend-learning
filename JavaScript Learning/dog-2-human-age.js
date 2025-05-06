405923e65006dea0892d5185b18a193c62b02edb
Reece Tunesi <rtunesi2000@gmail.com>
2025-05-06 15:13:25 +0100

    Converting dog years to human years program

---
+++ JavaScript Learning/dog-2-human-age.js
@@ -0,0 +1,8 @@
+// 1. Get users input of dog age.
+var dogAge = prompt("What is your dogs age?");
+
+// 2. Convert the dog age to human age.
+var humanAge = ((dogAge - 2) * 4) + 21;
+
+// 3. Display converted age to user
+alert("Your dog's age in human years is: " + humanAge);
