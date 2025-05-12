13a7d15eefab2c93f0627e1a769d01b002533656
Reece Tunesi <rtunesi2000@gmail.com>
2025-05-12 16:21:12 +0100

    Experimenting with Random

---
+++ JavaScript Learning/love-calc.js
@@ -0,0 +1,8 @@
+var usersName = prompt("What is your name?")
+var loversName = prompt("What is their name?")
+
+var n = Math.random();
+n = n * 100;
+n = Math.floor(n) + 1;
+
+console.log("Love percentage: " + n + "%");
---
+++ JavaScript Learning/random-number.js
@@ -0,0 +1,13 @@
+var number = Math.random();
+number = number * 6;
+number = Math.floor(number);
+
+console.log(number)
+
+// get ride of the 0
+
+var n = Math.random();
+n = n * 6;
+n = Math.floor(n) + 1;
+
+console.log(n)
