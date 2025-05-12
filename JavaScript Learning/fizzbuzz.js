aed5722ed0a7f334d00b59c5290a8a379b450420
Reece Tunesi <rtunesi2000@gmail.com>
2025-05-12 17:51:30 +0100

    FizzBuzz project completion

---
+++ JavaScript Learning/fizzbuzz.js
@@ -0,0 +1,23 @@
+// if number is divisable by 3 > Fizz
+// if number is divisable by 5 > buzz
+// if number is divisable by 3 AND 5 > fizzbuzz
+
+var output = [];
+var count = 1;
+
+function fizzBuzz(){
+    if (count % 5 === 0 && count % 3 === 0){
+        output.push("FizzBuzz")
+    }
+    else if (count % 5 === 0){
+        output.push("Buzz")
+    }
+    else if (count % 3 === 0){
+        output.push("Fizz")
+    }
+    else{
+        output.push(count);
+    }
+    count++;
+    console.log(output);
+}
