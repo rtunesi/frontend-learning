7ece90e5cab01fff95ce8b1da1758f1e3b0c1b38
Reece Tunesi <rtunesi2000@gmail.com>
2025-05-12 14:18:21 +0100

    Successfully completed second problem

---
+++ JavaScript Learning/jsbc-problem2.js
@@ -0,0 +1,6 @@
+var userAge = input("What is your age?");
+var yearsLeft = 90 - userAge;
+var monthsLeft = yearsLeft * 12;
+var weeksLeft = yearsLeft * 52;
+var daysLeft = yearsLeft * 365;
+console.log("You have " + daysLeft + " days, " + weeksLeft + " weeks and " + monthsLeft + " months left.")
