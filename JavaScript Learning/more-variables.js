f6dcb1656086fe2ba77d779a359e61c2a92557c9
Reece Tunesi <rtunesi2000@gmail.com>
2025-05-12 11:39:01 +0100

    Experimenting with functions

---
+++ JavaScript Learning/more-variables.js
@@ -0,0 +1,12 @@
+function buyMilk(number){
+    console.log("Buy " + number + " bottles of milk");
+}
+
+buyMilk(10);
+
+function milkTotal(amount){
+    totalAmount = amount * 1.5;
+    alert("Total price: £" + totalAmount)
+}
+
+milkTotal(2);
