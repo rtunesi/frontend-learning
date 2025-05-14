80ee4a21e5018e6ac73f4cfd6f9a86059e31f3df
Reece Tunesi <rtunesi2000@gmail.com>
2025-05-14 10:50:16 +0100

    Amended fibonacci generator code

--- JavaScript Learning/fibonacci.js
+++ JavaScript Learning/fibonacci.js
@@ -3,9 +3,27 @@ function fibonacci(n){
     var y = 1;
     var z = 0;
     for (count = 0; count < n; count++){
+
         z = x + y;
         console.log(z);
         x = y;
         y = z;
     }
+}
+
+function fibonacciGenerator(n){
+    var output = [];
+    if (n === 1){
+        output = [0];
+    }
+    else if (n === 2){
+        output = [0, 1];
+    }
+    else{
+        output = [0, 1];
+        for (var i = 2; i < n; i++){
+            output.push(output[output.length - 2] + output[output.length - 1]);
+        }
+    }
+    return output
 }
