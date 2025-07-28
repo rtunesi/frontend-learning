c662ff389c4f2c63067becffb3afa72440e479ee
Reece Tunesi <rtunesi2000@gmail.com>
2025-07-28 21:06:57 +0100

    Added basic check to javascript

---
+++ Udemy Course Files/Dicee+Challenge+-+Starting+Files/index.js
@@ -0,0 +1,45 @@
+var randomNumber1 = Math.random();
+randomNumber1 = randomNumber1 * 6;
+randomNumber1 = Math.floor(randomNumber1) + 1;
+
+var randomNumber2 = Math.random();
+randomNumber2 = randomNumber2 * 6;
+randomNumber2 = Math.floor(randomNumber2) + 1;
+
+if (randomNumber1 === 1){
+    document.getElementById("img1");
+}
+else if (randomNumber1 === 2){
+
+}
+else if (randomNumber1 == 3){
+    
+}
+else if (randomNumber1 == 4){
+    
+}
+else if (randomNumber1 == 5){
+    
+}
+else if (randomNumber1 == 6){
+    
+}
+
+if (randomNumber2 === 1){
+
+}
+else if (randomNumber2 === 2){
+
+}
+else if (randomNumber2 === 3){
+    
+}
+else if (randomNumber2 === 4){
+    
+}
+else if (randomNumber2 === 5){
+    
+}
+else if (randomNumber2 === 6){
+    
+}
