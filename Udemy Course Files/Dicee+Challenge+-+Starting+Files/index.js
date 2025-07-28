4845d8a6f74c9767cfdcb589320d40fe78317e93
Reece Tunesi <rtunesi2000@gmail.com>
2025-07-28 21:25:31 +0100

    Amended JavaScript if statements

--- Udemy Course Files/Dicee+Challenge+-+Starting+Files/index.js
+++ Udemy Course Files/Dicee+Challenge+-+Starting+Files/index.js
@@ -7,40 +7,40 @@ randomNumber2 = randomNumber2 * 6;
 randomNumber2 = Math.floor(randomNumber2) + 1;
 
 if (randomNumber1 === 1){
-    document.getElementById("img1");
+    document.getElementById("img1").setAttribute("src, images/dice1.png");
 }
 else if (randomNumber1 === 2){
-
+    document.getElementById("img1").setAttribute("src, images/dice2.png");
 }
 else if (randomNumber1 == 3){
-    
+    document.getElementById("img1").setAttribute("src, images/dice3.png");
 }
 else if (randomNumber1 == 4){
-    
+    document.getElementById("img1").setAttribute("src, images/dice4.png");
 }
 else if (randomNumber1 == 5){
-    
+    document.getElementById("img1").setAttribute("src, images/dice5.png");
 }
 else if (randomNumber1 == 6){
-    
+    document.getElementById("img1").setAttribute("src, images/dice6.png");
 }
 
 
 if (randomNumber2 === 1){
-
+    document.getElementById("img2").setAttribute("src, images/dice1.png");
 }
 else if (randomNumber2 === 2){
-
+    document.getElementById("img2").setAttribute("src, images/dice2.png");
 }
 else if (randomNumber2 === 3){
-    
+    document.getElementById("img2").setAttribute("src, images/dice3.png");
 }
 else if (randomNumber2 === 4){
-    
+    document.getElementById("img2").setAttribute("src, images/dice4.png");
 }
 else if (randomNumber2 === 5){
-    
+    document.getElementById("img2").setAttribute("src, images/dice5.png");
 }
 else if (randomNumber2 === 6){
-    
+    document.getElementById("img2").setAttribute("src, images/dice6.png");
 }
